3cd1000c158ea0c45ba6bed3f8d075506fecd989
Alucard0x1 <lombokweb@gmail.com>
2024-09-05 20:21:51 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -71,26 +71,29 @@ $result = null;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $num1 = isset($_POST['num1']) ? (float)$_POST['num1'] : 0;
     $num2 = isset($_POST['num2']) ? (float)$_POST['num2'] : 0;
-    $operation = $_POST['operation'];
+    $operation = isset($_POST['operation']) ? $_POST['operation'] : null;
     
-    switch ($operation) {
-        case 'add':
-            $result = $num1 + $num2;
-            break;
-        case 'subtract':
-            $result = $num1 - $num2;
-            break;
-        case 'multiply':
-            $result = $num1 * $num2;
-            break;
-        case 'divide':
-            $result = ($num2 != 0) ? $num1 / $num2 : "Error (Division by 0)";
-            break;
-        default:
-            $result = "Invalid Operation!";
+    // Only perform calculation if an operation is selected
+    if ($operation) {
+        switch ($operation) {
+            case 'add':
+                $result = $num1 + $num2;
+                break;
+            case 'subtract':
+                $result = $num1 - $num2;
+                break;
+            case 'multiply':
+                $result = $num1 * $num2;
+                break;
+            case 'divide':
+                $result = ($num2 != 0) ? $num1 / $num2 : "Error (Division by 0)";
+                break;
+            default:
+                $result = "Invalid Operation!";
+        }
+        
+        $display = $result;
     }
-    
-    $display = $result;
 }
 ?>
 
